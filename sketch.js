e45d455aa138966e3aa965e5b129460347888135
n3ptun1a <dng4939@nyu.edu>
2025-04-30 00:16:48 -0400

    a
    im gonna be honest idek if this code works!!! im so confused on what isnt letting me display this in my browser so i need to ask some questions in class! apologies lady k

--- sketch.js
+++ sketch.js
@@ -7,16 +7,22 @@ let unlockedLevels = [true, false, false];
 
 let currentLevel = null;
 let platforms = [];
+let snowflakes = [];
+let fireCinders = [];
 
 let ball;
+let victorySound;
 
 function setup() {
-createCanvas(800, 600);
-
-mic = new p5.AudioIn();
-mic.start();
-
-ball = new Player();
+  createCanvas(800, 600);
+  
+  mic = new p5.AudioIn();
+  mic.start();
+  
+  ball = new Player();
+  
+  // Load sound for victory
+  victorySound = loadSound('data/victorySound.mp3');
 }
 
 function draw() {
@@ -89,20 +95,45 @@ function startLevel(n){
 
   platforms = [];
   if(n == 1){
-    platforms.push(new Platform(0, 550, 800, 50));
+    platforms.push(new Platform(0, 550, 800, 50)); // ground
+  } else if(n == 2){
+    platforms.push(new Platform(0, 550, 800, 50)); // ground
+  } else if(n == 3){
+    platforms.push(new Platform(0, 550, 800, 50)); // ground
+  }
+
+  // Add snowflakes or fire cinders
+  if(n == 2){
+    for (let i = 0; i < 50; i++){
+      snowflakes.push(new Snowflake());
+    }
+  } else if(n == 3){
+    for (let i = 0; i < 50; i++){
+      fireCinders.push(new FireCinder());
+    }
   }
 }
 
 function playGame(){
   micLevel = mic.getLevel();
-  pitch = micLevel * 500;
+  pitch = mic.getLevel() * 500;
 
   if(currentLevel == 1){
-    background(150,200,255);
+    background(150,200,255); // blue sky
   } else if(currentLevel == 2){
-    background(255);
+    background(255); // white for snow
+    // Snow effect for 1-2
+    for (let snowflake of snowflakes){
+      snowflake.update();
+      snowflake.show();
+    }
   } else {
-    background(255,80,80);
+    background(255,80,80); // red for fire
+    // Fire cinders for 1-3
+    for (let fire of fireCinders){
+      fire.update();
+      fire.show();
+    }
   }
 
   ball.update();
@@ -127,7 +158,6 @@ function drawMicUI(){
   rect(20 + 75, 50, (pitch - 128)/128 * 75, 20);
 }
 
-// this is the player class aka the ball
 class Player {
   constructor(){
     this.reset();
@@ -149,19 +179,17 @@ class Player {
       this.vx *= 0.9;
     }
 
-    // check pitch jump / stick
     if(pitch < 100){
       this.vy = -10;
-      this.color = color(0,0,255);
+      this.color = color(0,0,255); // blue
     } else if(pitch > 200){
       this.sticky = true;
-      this.color = color(0,255,0);
+      this.color = color(0,255,0); // green
     } else {
       this.sticky = false;
-      this.color = color(255,255,0);
+      this.color = color(255,255,0); // yellow
     }
 
-    // gravity unless stuck
     if(!this.sticky){
       this.vy += 0.5;
     }
@@ -174,6 +202,9 @@ class Player {
       if(this.x > p.x && this.x < p.x + p.w && this.y + 10 > p.y && this.y + 10 < p.y + p.h){
         this.y = p.y - 10;
         this.vy = 0;
+        if(p.x > 700){
+          this.winLevel();
+        }
       }
     }
 
@@ -195,6 +226,12 @@ class Player {
       text("Speak up!", this.x, this.y - 40);
     }
   }
+
+  winLevel(){
+    victorySound.play();
+    unlockedLevels[currentLevel] = true;
+    currentScreen = "levelSelect";
+  }
 }
 
 class Platform {
@@ -208,5 +245,52 @@ class Platform {
   show(){
     fill(100);
     rect(this.x, this.y, this.w, this.h);
+  }
+}
+
+// Snowflakes for level 1-2
+class Snowflake {
+  constructor(){
+    this.x = random(width);
+    this.y = random(-500, -50);
+    this.size = random(5, 10);
+    this.speed = random(1, 3);
+  }
+
+  update(){
+    this.y += this.speed;
+    if(this.y > height){
+      this.y = random(-500, -50);
+    }
+  }
+
+  show(){
+    fill(255);
+    noStroke();
+    ellipse(this.x, this.y, this.size, this.size);
+  }
+}
+
+// Fire cinders for level 1-3
+class FireCinder {
+  constructor(){
+    this.x = random(width);
+    this.y = random(-500, -50);
+    this.size = random(2, 5);
+    this.speed = random(2, 4);
+    this.color = color(random(255), random(100, 200), 0);
+  }
+
+  update(){
+    this.y += this.speed;
+    if(this.y > height){
+      this.y = random(-500, -50);
+    }
+  }
+
+  show(){
+    fill(this.color);
+    noStroke();
+    ellipse(this.x, this.y, this.size, this.size);
   }
 }
