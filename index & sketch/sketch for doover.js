17e3c10cb89ed408d3c600b2c676d6d95bb8deb3
n3ptun1a <dng4939@nyu.edu>
2025-05-12 13:19:58 -0400

    setting up
    really just hoping i can get this done in time for the reflection

--- index & sketch/sketch for doover.js
+++ index & sketch/sketch for doover.js
@@ -1,1 +1,20 @@
-let
+// lets go golfing - dj khaled
+// uses p5.Vector, localStorage (according to research)
+
+// globals
+let levels = [];
+let currentLevel = 0;
+let ball;
+let holePos;
+let wind;            // p5.Vector
+let wells = [];      // array of Well
+let shots, bestShots = {};
+
+// aiming
+let isAiming = false;
+
+function setup() {
+	createCanvas(800, 600);
+	loadBest();
+	initLevel(0);
+}
